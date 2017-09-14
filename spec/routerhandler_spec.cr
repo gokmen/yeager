83aba6a0d6d4b5f9cb4bf592620e82fec8d244a1
Gokmen Goksel <gokmen@goksel.me>
2017-09-14 01:29:23 -0700

    Spec: RouterHandler spec for supported features

---
+++ spec/routerhandler_spec.cr
@@ -0,0 +1,149 @@
+require "./spec_helper"
+
+module Yeager
+  private alias ProcType = String -> Nil
+
+  describe RouterHandler do
+    it "should be able to initialize with provided generic type" do
+      r = Yeager::RouterHandler(ProcType).new
+      r.class.should eq(Yeager::RouterHandler(ProcType))
+      r.routes.class.should eq(Yeager::Routes)
+      r.handlers.class.should eq(Hash(String, ProcType))
+    end
+
+    it "should call actions with no arguments" do
+      r = Yeager::RouterHandler(-> Nil).new
+
+      called = false
+
+      r.add "/", ->{ called = true }
+      r.routes.should eq({"/" => ["/"]})
+
+      r.run("/").should eq({:path => "/"})
+      called.should be_true
+
+      called = false
+
+      r.run("/foo").should be_nil
+      called.should be_false
+    end
+
+    it "should call actions with provided arguments" do
+      r = Yeager::RouterHandler(ProcType).new
+
+      called = false
+
+      r.add "/", ->(name : String) {
+        called = true
+        name.should eq("test")
+      }
+
+      r.routes.should eq({"/" => ["/"]})
+
+      r.run("/", "test").should eq({:path => "/"})
+
+      called.should be_true
+    end
+
+    it "should support multiple arguments with union types" do
+      r = Yeager::RouterHandler(String, Int32 -> Nil).new
+
+      called = false
+
+      r.add "/", ->(name : String, id : Int32) {
+        called = true
+        name.should eq("test")
+        id.should eq(123)
+      }
+
+      r.routes.should eq({"/" => ["/"]})
+
+      r.run("/", "test", 123).should eq({:path => "/"})
+
+      called.should be_true
+    end
+
+    it "should pass parameter values if requested" do
+      r = Yeager::RouterHandler(Yeager::Result -> Nil).new
+
+      called = false
+
+      r.add "/:name", ->(params : Yeager::Result) {
+        called = true
+        params["name"].should eq("test")
+      }
+
+      r.routes.should eq({"/:name" => [":name"]})
+
+      r.run("/test", params = true).should eq({:path => "/:name", "name" => "test"})
+
+      called.should be_true
+    end
+
+    it "should pass emptied parameters if requested" do
+      r = Yeager::RouterHandler(Yeager::Result -> Nil).new
+
+      called = false
+
+      r.add "/:name", ->(params : Yeager::Result) {
+        called = true
+        params.empty?.should be_true
+      }
+
+      r.routes.should eq({"/:name" => [":name"]})
+
+      r.run("/test", params = false).should eq({:path => "/:name", "name" => "test"})
+
+      called.should be_true
+    end
+
+    it "should pass parameter values with arguments if requested" do
+      r = Yeager::RouterHandler(Yeager::Result, String -> Nil).new
+
+      called = false
+
+      r.add "/:name", ->(params : Yeager::Result, test : String) {
+        called = true
+        params.should eq({
+          :path  => "/:name",
+          "name" => "test",
+        })
+        test.should eq("Hello")
+      }
+
+      r.routes.should eq({
+        "/:name" => [":name"],
+      })
+
+      r.run("/test", params = true, "Hello").should eq({
+        :path  => "/:name",
+        "name" => "test",
+      })
+
+      called.should be_true
+    end
+
+    it "should pass emptied parameter values with arguments if requested" do
+      r = Yeager::RouterHandler(Yeager::Result, String -> Nil).new
+
+      called = false
+
+      r.add "/:name", ->(params : Yeager::Result, test : String) {
+        called = true
+        params.empty?.should be_true
+        test.should eq("Hello")
+      }
+
+      r.routes.should eq({
+        "/:name" => [":name"],
+      })
+
+      r.run("/test", params = false, "Hello").should eq({
+        :path  => "/:name",
+        "name" => "test",
+      })
+
+      called.should be_true
+    end
+  end
+end
