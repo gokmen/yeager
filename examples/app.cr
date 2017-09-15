5bc0da774ee0d350747391069c4f8f1a4bb8bde8
Gokmen Goksel <gokmen@goksel.me>
2017-09-15 00:04:18 -0700

    Examples: App: an example usage of Yeager::App

---
+++ examples/app.cr
@@ -0,0 +1,29 @@
+require "../src/yeager"
+
+app = Yeager::App.new
+
+app.get "/" do |req, res|
+  res.send "Hello world!"
+end
+
+app.post "/" do |_, res|
+  res.status(200).json({"Hello" => "world!"})
+end
+
+app.get "/:name" do |req, res|
+  res.send "Hello sub world! #{req.params["name"]}"
+end
+
+# If you have a defined HTTP::Server already you can
+# use app.handler after this point instead of running
+# the server of the app.
+app.listen 3000 do |url|
+  print "Example app listening on #{url}!"
+end
+
+# Example usage for using the app.handler
+# with an existing server
+#
+# server = HTTP::Server.new("0.0.0.0", 8080, [app.handler])
+# puts "Listening on 0.0.0.0:8080..."
+# server.listen
